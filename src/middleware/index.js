9799d73c79616c1b0fa52fde544d9034fbbb4b74
wahyuindra712 <wahyuindra712@gmail.com>
2024-01-12 19:08:46 +0700

    fix get user token

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,22 @@
 import jwt from 'jsonwebtoken';
+import { sendUnauthorized } from '../utils/helper.js';
 
 export const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Access denied' });
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid token' });
-        req.user = user;
+    if (!token) {
+        console.log('No token provided');
+        return sendUnauthorized(res, 'Access denied, no token provided');
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+        if (err) {
+            console.log('Invalid token:', err.message);
+            return sendUnauthorized(res, 'Invalid token');
+        }
+        req.user = decode;
+        console.log('Token verified successfully');
         next();
     });
-}
+};
