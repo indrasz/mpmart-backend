9074679b04ca8274db635cec50f651c84d1ecf09
wahyuindra712 <wahyuindra712@gmail.com>
2024-01-12 19:06:44 +0700

    fix configuration

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
