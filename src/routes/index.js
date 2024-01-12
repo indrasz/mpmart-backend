9f5fc1df01549e8a073ce1b05b7c08536c310a36
wahyuindra712 <wahyuindra712@gmail.com>
2024-01-12 19:07:52 +0700

    update route & add endpoint user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js'
+import { register, login, logout } from '../controllers/authController.js'
+import { getUserProfile } from '../controllers/userController.js';
+import { authenticateToken } from '../middleware/index.js';
 
 const router = express.Router();
+const url = process.env.API_URL;
 
-router.post('/mp-mart/api/v1/auth/register', register);
-router.post('/mp-mart/api/v1/auth/login', login);
+// Route for auth
+router.post(`${url}/auth/register`, register);
+router.post(`${url}/auth/login`, login);
+router.post(`${url}/auth/logout`, logout);
+
+//Route for user
+router.get(`${url}/profile`, authenticateToken, getUserProfile);
 
 export default router;
