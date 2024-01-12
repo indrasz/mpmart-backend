314c2d10fb0be644f34e4316f7e223e5591029c0
wahyuindra712 <wahyuindra712@gmail.com>
2024-01-12 19:08:14 +0700

    add findUserById function

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,10 @@ export const createUser = async (data) => {
     return prisma.user.create({ data });
 }
 
+export const findUserById = async (id) => {
+    return prisma.user.findUnique({ where: { id } });
+}
+
 export const findUserByUsername = async (username) => {
     return prisma.user.findUnique({ where: { username } });
 }
